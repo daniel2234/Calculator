7268a55282ae09da781d079da2ce4eebeb67d8d2
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-06-12 21:25:59 -0400

    added a private method to do te squareroot

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -35,12 +35,13 @@ class ViewController: UIViewController {
         case "÷":performOperation({ $1 / $0 })
         case "−":performOperation({ $1 - $0 })
         case "+":performOperation({ $0 + $1 })
+        case "": performOperation({sqrt($0)})
         default:break
         }
         
     }
     
-    func performOperation(operation:(Double,Double) -> Double){
+    func performOperation(operation: (Double, Double) -> Double){
         if operandStack.count >= 2{
             //removes everything from the array once it appends
             displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
@@ -48,6 +49,14 @@ class ViewController: UIViewController {
         }
     }
     
+    
+   private func performOperation(operation: Double -> Double){
+        if operandStack.count >= 1{
+            displayValue = operation(operandStack.removeLast())
+        }
+    }
+    
+
     // make and internal stack of numbers
     var operandStack = Array<Double>()
     
